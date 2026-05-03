3a704effac3f1cf524d98d76c129f64bae8ff72b
Martí Forn <magiclovett@gmail.com>
2026-05-03 23:58:07 +0200

    Add splitting data

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.preprocessing import preprocess_data
+from src.split import split_data
 
 def main():
     X, y = preprocess_data()
+    X_train, X_test, y_train, y_test = split_data(X, y)
 
 if __name__ == "__main__":
     main()
--- src/split.py
+++ src/split.py
@@ -1,2 +1,11 @@
-def split_data(X, y):
+import pandas as pd
+
+def split_data(X: pd.DataFrame, y: pd.Series, train_ratio: float = 0.8): 
+    train_size = int(train_ratio * len(X))
+
+    X_train = X.iloc[:train_size]
+    y_train = y.iloc[:train_size]
+    X_test = X.iloc[train_size:]
+    y_test = y.iloc[train_size:]
+
     return X_train, X_test, y_train, y_test
