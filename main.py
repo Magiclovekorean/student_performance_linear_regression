fa628c5d49739ea83750f06949f6415b5ea83b62
Martí Forn <magiclovett@gmail.com>
2026-05-04 09:57:25 +0200

    Fix bug in typing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.preprocessing import preprocess_data
 from src.split import split_data
+from config import LEARNING_RATE, TRAIN_RATIO
 
 def main():
     X, y = preprocess_data()
-    X_train, X_test, y_train, y_test = split_data(X, y)
+    X_train, X_test, y_train, y_test = split_data(X, y, TRAIN_RATIO)
 
 if __name__ == "__main__":
     main()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 def preprocess_data():
     df = pd.read_csv("data/dataset.csv")
     X = df.drop(["student_id", "participation_score", "assignment_score", "midterm_score", "final_exam_score", "overall_score", "grade"], axis=1)
-    y = df[["overall_score"]]
+    y = df["overall_score"]
     X = pd.get_dummies(X, dtype=int)
     return X, y
 
