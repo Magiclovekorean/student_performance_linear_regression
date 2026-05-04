Fixed  some  bugs, normalization   needed!

main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from config import LEARNING_RATE, TRAIN_RATIO, GRADIENT_DESCENT_ITERS
 from src.preprocessing import preprocess_data
 from src.split import split_data
@@ -6,7 +8,8 @@ from src.train import train_model
 def main():
     X, y = preprocess_data()
     X_train, X_test, y_train, y_test = split_data(X, y, TRAIN_RATIO)
-    print(train_model(X_train, 0, 0, y_train, LEARNING_RATE, GRADIENT_DESCENT_ITERS))
+    initial_w = np.zeros(X_train.shape[1])
+    print(train_model(X_train, initial_w, 0, y_train, LEARNING_RATE, GRADIENT_DESCENT_ITERS))
 
 if __name__ == "__main__":
     main()

src/train.py
@@ -1,23 +1,24 @@
 import numpy as np
 from src.model import predict_value
 
-def train_model(x, w, b, y, alpha, iterations):
+def train_model(X, w, b, y, alpha, iterations):
     for i in range(iterations):
-        dj_dw, dj_db = compute_gradient(x, w, b, y)
+        dj_dw, dj_db = compute_gradient(X, w, b, y)
         w = w - alpha * dj_dw
         b = b - alpha * dj_db
         
 
-    f_wb = x * w + b
+    f_wb = predict_value(X, w, b)
     error = f_wb - y
     return error
     
 
-def compute_gradient(x, w, b, y):
-    f_wb = x * w + b
+def compute_gradient(X, w, b, y):
+    f_wb = predict_value(X, w, b)
     error = f_wb - y
-    
-    dj_dw  = np.sum(error * x)
-    dj_db  = np.sum(error)
+
+    m = X.shape[0]
+    dj_dw  = X.T @ error / m
+    dj_db  = np.sum(error) / m
 
     return dj_dw, dj_db